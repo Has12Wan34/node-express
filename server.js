9412e2331f3484c28a6ab203e32915bdd5989d8c
Has12Wan34 <94791694+Has12Wan34@users.noreply.github.com>
2024-05-26 13:58:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var app = express();
 const userRouter = require('./routes/user');
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.header('Access-Control-Allow-Headers', "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+  return;
+  };
+  next();
+});
+
 app.use(express.json())
 app.use('/api/user', userRouter);
 
