33b75a799d97ffdc74fd5ea4fb9a7164b9ca3a38
Has12Wan34 <rbae1998@gmail.com>
2024-06-03 20:48:47 +0700

    fix corsss

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ var app = express();
 
 const userRouter = require('./routes/user');
 
-app.use(cors())
+// app.use(cors())
+const issue2options = {
+  origin: true,
+  methods: ["POST"],
+  credentials: true,
+  maxAge: 3600
+};
+app.options("/api/user", cors(issue2options));
 app.use(express.json())
 app.use('/api/user', userRouter);
 
